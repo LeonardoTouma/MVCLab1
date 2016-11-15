48d9969a6600eddfaa590bc434325466b347ae60
Leonardo <toumaleonardo@gmail.com>
2016-11-15 23:46:01 +0100

    Photo class in model added.

---
+++ MvcLab1.0/MvcLab1.0/Models/Photo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcLab1._0.Models
+{
+    public class Photo
+    {
+        public Guid Id{ get; set; }
+        public string Photoname { get; set; }
+        public string Path { get; set; }
+        public string AlternateText { get; set; }
+    }
+}
